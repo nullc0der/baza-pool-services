ce378cc1efc77cbb47f3534269d176ae8a8f45b8
Prasanta Kakati <prasantakakati1994@gmail.com>
2020-10-23 02:40:35 +0530

    [ADDED] Next session generation

--- votingsessions/utils.py
+++ votingsessions/utils.py
@@ -1,4 +1,4 @@
-from django.utils.timezone import now
+from django.utils.timezone import now, timedelta
 
 from votingsessions.models import VotingSession
 
@@ -6,6 +6,14 @@ from votingsessions.models import VotingSession
 def get_current_session():
     voting_sessions = VotingSession.objects.filter(
         start_date__lte=now(), end_date__gt=now())
+    if not voting_sessions:
+        next_sessions = VotingSession.objects.filter(
+            start_date__gt=now()
+        )
+        current_session = next_sessions[0]
+        current_session.start_date = now().date()
+        current_session.save()
+        return current_session
     return voting_sessions[0]
 
 
@@ -13,4 +21,11 @@ def get_next_session():
     current_session = get_current_session()
     next_sessions = VotingSession.objects.filter(
         start_date__gt=current_session.end_date)
+    if not next_sessions:
+        next_session = VotingSession.objects.create(
+            start_date=current_session.end_date + timedelta(days=1),
+            end_date=current_session.end_date + timedelta(days=31),
+            minimum_amount_per_token=4000
+        )
+        return next_session
     return next_sessions[0]
--- votingsessions/views.py
+++ votingsessions/views.py
@@ -1,11 +1,12 @@
 from operator import itemgetter
 
-from tokendb.models import Token
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+
+from tokendb.models import Token
 
 from votingsessions.models import VotingSession
 from votingsessions.serializers import VotingSessionSerializer
